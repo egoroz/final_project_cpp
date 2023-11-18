4dffbf18aa199c4e00eb8d6485fc5b88b2ea2a59
egoroz <egorsemirenko04@gmail.com>
2023-11-19 00:34:06 +0300

    hero appeared! and move!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <SFML/Graphics.hpp>
+//TODO audio
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "name project"); 
+    sf::Image heroimage;
+    heroimage.loadFromFile("images/heroes/soviet_man.png");
+
+    sf::Texture herotexture;
+    herotexture.loadFromImage(heroimage);
+
+    sf::Sprite herosprite;
+    herosprite.setTexture(herotexture);
+    herosprite.setTextureRect(sf::IntRect(150, 30, -150, 170)); //first way unfold the charecter - to use negative coords
+    herosprite.setPosition(0, 0);
 
     while (window.isOpen())
     {
@@ -15,8 +24,14 @@ int main()
                 window.close();
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))  {herosprite.move(-1.1, 0); herosprite.setTextureRect(sf::IntRect(150, 30, -150, 170)); }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {herosprite.move(1.1, 0);  herosprite.setTextureRect(sf::IntRect(0, 30, 150, 170));}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))    {herosprite.move(0, -1.1); herosprite.setTextureRect(sf::IntRect(150, 30, -150, 170));}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))  {herosprite.move(0, 1.1);  herosprite.setTextureRect(sf::IntRect(0, 30, 150, 170));}
+
+
         window.clear();
-        window.draw(shape);
+        window.draw(herosprite);
         window.display();
     }
 
