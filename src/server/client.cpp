29bb0114467aa3d6d46b1c8f2d55e1975e132abc
SavoSya <savostikov.artem@gmail.com>
2023-12-11 19:44:00 +0300

    client modified

--- src/server/client.cpp
+++ src/server/client.cpp
@@ -6,11 +6,11 @@ int main()
 {
     // Создаем UDP сокет на клиенте
     sf::UdpSocket clientSocket;
-    unsigned short clientPort = 55002;
+    unsigned short clientPort = 55005;
     clientSocket.bind(clientPort);
 
     // Определяем IP-адрес и порт сервера
-    sf::IpAddress serverAddress = "192.168.33.76";
+    sf::IpAddress serverAddress = "192.168.1.105";
     unsigned short serverPortNumber = 55001;
 
     while (true)
@@ -25,6 +25,26 @@ int main()
         {
             keyCode = 'A';
         }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            keyCode = 'W';
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            keyCode = 'S';
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
+        {
+            keyCode = 'G';
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+        {
+            keyCode = 'F';
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            keyCode = 'Z';
+        }
 
         // Отправляем код клавиши на сервер
         char message[2];
@@ -36,12 +56,11 @@ int main()
         char buffer[1024];
         std::size_t received;
         clientSocket.receive(buffer, sizeof(buffer), received, serverAddress, serverPortNumber);
-        std::cout << "Key code received from server: " << static_cast<int>(buffer[0]) << std::endl;
+        std::cout << "Key code received from server: " << buffer[0] << std::endl;
 
         // Проверяем, нажата ли клавиша Escape
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        {
-            break;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+            return 0;
         }
     }
 
