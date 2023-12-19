8cd57c062776e6c841e94f59b5bc06c0a0713bc0
SavoSya <savostikov.artem@gmail.com>
2023-12-19 13:07:53 +0300

    target added

--- include/target.hpp
+++ include/target.hpp
@@ -6,20 +6,20 @@
 class Target {
 private:
     float x, y, w, h;
-    sf::RectangleShape rectangle;
+    sf::RectangleShape* rectangle = new sf::RectangleShape {sf::Vector2f(100, 100)};
 
 public:
     Target(){
         int x=1050; int y=1040; int w=100; int h=100;
-        sf::RectangleShape rectangle(sf::Vector2f(w, h));
-        rectangle.setFillColor(sf::Color::Red);
-        rectangle.setPosition(x, y);
+        //rectangle->setSize(w, h);
+        rectangle->setFillColor(sf::Color::Red);
+        rectangle->setPosition(x, y);
     }
 
     sf::FloatRect getRect(){ return sf::FloatRect(x, y, w, h);}
 
     void draw(sf::RenderWindow& window){
-        window.draw(rectangle);
+        window.draw(*rectangle);
     }
 };
 
